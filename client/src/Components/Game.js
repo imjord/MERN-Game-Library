c5b0e685304c9fb5ab59c446875516bf2b29dc86
imjord <dean.jay.goat@gmail.com>
2022-07-20 16:43:25 -0600

    able to visit a single game details page and styled that page a little

--- client/src/Components/Game.js
+++ client/src/Components/Game.js
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react'
 import {useParams} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
 const Game = (props) => {
   const {singleGame, loading, setLoading, getSingleGame} = props;
@@ -8,10 +10,29 @@ const Game = (props) => {
   getSingleGame(id);
   },[]);
   return (
-    <div>
-      <h1>Game</h1>
-      <p>{singleGame.name}</p>
+    <div className='single-game'>
 
+      <div className='single-title'>
+        <div>
+        <h1>{singleGame.name}</h1>
+        </div>
+        </div>  
+      <div className='right-aside'>
+        <div>
+        <h4>Description:</h4>
+         {singleGame.description}
+         <h4>Release Date:</h4>
+          <p>{singleGame.releaseDate}</p>
+        </div>
+        </div>
+      <div className='trailer'>
+        <div>
+        <h4>Trailer</h4>
+        <iframe width="560" height="315" src={singleGame.trailer} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        
+        </div>
+        </div>
+      
     </div>
   )
 }
--- server/config/seeds.js
+++ server/config/seeds.js
@@ -12,25 +12,37 @@ const seedProducts = [
         name: 'Doom',
         price: 10,
         category: 'FPS',
-        image: 'doom.jpg'
+        description: 'Doom is a first-person shooter video game, developed by id Software and published by Bethesda Softworks. It is the fourth installment in the Doom series, and the successor to Doom II: Hell on Earth. Doom is a single-player, three-dimensional action-adventure game, and the first game in the Doom series to be released for Microsoft Windows. It was released in May 2016 for Microsoft Windows, PlayStation 4, and Xbox One. Doom is a first-member of the Doom series, and the first game in the series to be released for the Nintendo Switch.',
+        image: 'doom.jpg',
+        trailer: 'https://www.youtube.com/embed/BkaC1-QoraY',
+        releaseDate: '1993-12-10'
     },
     {
         name: 'Mario',
         price: 10,
         category: 'Platformer',
-        image: 'mario.jpg'
+        description: 'Mario is a platform game',
+        image: 'mario.jpg',
+        trailer: 'https://www.youtube.com/embed/eO8xe2AUY4c',
+        releaseDate: '2012-07-28'
     },
     {
         name: 'Pokemon',
         price: 10,
         category: 'RPG',
-        image: 'pokemon.jpg'
+        description: 'Pokemon is a role-playing video game series created by Satoshi Tajiri and first released in 1996 for the Game',
+        image: 'pokemon.jpg',
+        trailer: 'https://www.youtube.com/embed/I4RynqpahT8',
+        releaseDate: '2022-01-28'
     },
     {
         name: 'Grand Theft Auto V',
         price: 60,
         category: 'Action',
-        image: 'gta.jpg'
+        description: "Grand Theft Auto V is an open world action-adventure video game developed by Rockstar North and published by Rockstar Games. It was released on September 9, 2013 for the PlayStation 3 and Xbox 360, and on November 4, 2013 for the PlayStation 4 and Xbox One. It is the eleventh installment in the Grand Theft Auto series.",
+        image: 'gta.jpg',
+        trailer: 'https://www.youtube.com/embed/QkkoHAzjnUs',
+        releaseDate: '2013-09-17'
     }
 ]
 
