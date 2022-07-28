81c3050ef3ee3fba1c1e21f5bb6a2ba72bf7bf75
imjord <dean.jay.goat@gmail.com>
2022-07-27 23:03:33 -0600

    commit before trying passport

--- client/src/pages/Library.js
+++ client/src/pages/Library.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const Library = () => {
+  const [msg, setMsg] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/users/library')
+      .then(response => {
+        console.log(response.data.message)
+        setMsg(response.data.message)
+      }).catch(err => {
+        console.log(err)
+        console.log(err.response.data.message)
+        setMsg(err.response.data.message)
+      }
+    )
+  })
   return (
-    <div>Library</div>
+    <div>
+    {loggedIn ? <div> youre logged in </div> : <div>{msg}</div>}
+   </div>
   )
 }
 
