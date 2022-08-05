246e091532fe6f1a90cf52d089a9902ea2b44151
imjord <dean.jay.goat@gmail.com>
2022-08-04 19:07:05 -0600

    heroku work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/reactGameDB');
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 
 module.exports = mongoose.connection;
