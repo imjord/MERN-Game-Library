75301b74251c81c960cb5f56ba51ef422eed6bf2
imjord <dean.jay.goat@gmail.com>
2022-08-10 11:25:20 -0600

    public files for css01020304

--- server/server.js
+++ server/server.js
@@ -39,10 +39,7 @@ app.use(passport.session());
 
 app.use(cookieParser(('secret')));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-}
-);
+
 
 app.use('/api', GameRoutes);
 app.use('/api/users', UserRoutes);
@@ -51,7 +48,7 @@ app.use('/api/users', UserRoutes);
 
 if (process.env.NODE_ENV === "production") {
 
-    app.use(express.static('client/build'))  // set static folder 
+    app.use(express.static('../client/build'))  // set static folder 
     
     app.get('*', (req, res)=> {     
       res.sendFile(path.resolve(__dirname, 'client', 'build',         
