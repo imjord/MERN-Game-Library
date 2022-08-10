f424d79938cbd668b1bbe1c26df3eceaf351c881
imjord <dean.jay.goat@gmail.com>
2022-08-10 10:59:00 -0600

    added get * to build

--- server/server.js
+++ server/server.js
@@ -14,6 +14,11 @@ require('./config/passport')(passport);
 require('dotenv').config()
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    }
+    );
   }
 
 app.use(cors({
