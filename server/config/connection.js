76a2d704693306f5f765ba507d735cf7ae579eba
imjord <dean.jay.goat@gmail.com>
2022-08-10 10:56:27 -0600

    working main site works unless you try to refresh for some reason. env files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-mongoose.connect("mongodb+srv://imjord:imjord123@cluster0.igc6tbx.mongodb.net/?retryWrites=true&w=majority" || 'mongodb://localhost/reactGameDB',{
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/reactGameDB',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
