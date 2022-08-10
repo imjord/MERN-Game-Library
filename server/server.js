733e87492f348b8b4d3e88d27b2a56367d526237
imjord <dean.jay.goat@gmail.com>
2022-08-10 11:33:04 -0600

    public files for css010203040506

--- server/server.js
+++ server/server.js
@@ -45,21 +45,14 @@ app.use('/api', GameRoutes);
 app.use('/api/users', UserRoutes);
 
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+}
 
-if (process.env.NODE_ENV === "production") {
+);
 
-    app.use(express.static('../client/build'))  // set static folder 
-    
-    app.get('*', (req, res)=> {     
-      res.sendFile(path.resolve(__dirname, 'client', 'build',         
-                    'index.html' )); 
-    })
-    app.get('*', (req, res)=> {     
-      res.sendFile(path.resolve(__dirname, 'client', 'build',         
-                    'index.html' )); 
-    })
-  }
-
+  
 
 
 
