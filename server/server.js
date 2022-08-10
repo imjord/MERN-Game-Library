0dcd71f76ef211f934d84a4b2fbcd3000bbf1c4e
imjord <dean.jay.goat@gmail.com>
2022-08-10 11:20:51 -0600

    public files for css010203

--- server/server.js
+++ server/server.js
@@ -12,16 +12,7 @@ const passport = require('passport');
 const path = require('path');
 require('./config/passport')(passport);
 require('dotenv').config()
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    }
-    );
-    
-  }
-  app.use(express.static('../client/public'))
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
@@ -55,6 +46,25 @@ app.get('/', (req, res) => {
 
 app.use('/api', GameRoutes);
 app.use('/api/users', UserRoutes);
+
+
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static('client/build'))  // set static folder 
+    
+    app.get('*', (req, res)=> {     
+      res.sendFile(path.resolve(__dirname, 'client', 'build',         
+                    'index.html' )); 
+    })
+    app.get('*', (req, res)=> {     
+      res.sendFile(path.resolve(__dirname, 'client', 'build',         
+                    'index.html' )); 
+    })
+  }
+
+
+
 
 db.once('open', () => {
     console.log('Connected to MongoDB');
